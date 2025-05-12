13f3360c2448fcab9ea6cfa72ffb3983c3c882e3
garugan <196047075+garugan@users.noreply.github.com>
2025-05-12 09:53:38 +0000

    [Add]admin-dadhboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :admins, path: 'admin', controllers: {
+  sessions: 'admin/admins/sessions'
+}
+
   root to: "homes#top"
   post 'guest_login', to: 'users#guest_login'
 
   resources :games do
     resources :reviews
   end
+
   resources :users
   resources :genres
 
-  devise_for :admins, path: 'admin', controllers: {
-  sessions: 'admin/admins/sessions'
-}
+
+namespace :admin do
+  get 'dashboards', to: 'dashboards#index'
+end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
