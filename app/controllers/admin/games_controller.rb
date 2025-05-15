2986a309f8fb75ef7f75c2fe4ada73cf5a39b3d1
garugan <196047075+garugan@users.noreply.github.com>
2025-05-15 05:29:06 +0000

    [Add]admin_games1

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,2 +1,38 @@
 class Admin::GamesController < ApplicationController
+  layout 'admin'
+  before_action :authenticate_admin!
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to admin_games_path notice: "ゲームを追加しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to admin_games_path, notice: 'ゲームを削除しました。'
+  end
+
+  def index
+    @games = Game.all
+  end
+
+private
+
+def game_params
+  params.require(:game).permit(:title, :body, :genre_id)
+end
+
 end
