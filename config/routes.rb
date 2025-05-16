884e020a0c5adc73cf6fc8dfe8200e5ab094b10e
garugan <196047075+garugan@users.noreply.github.com>
2025-05-16 05:41:06 +0000

    [Add]admin_nav

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,5 @@ devise_for :admins, path: 'admin', controllers: {
     resources :reviews
   end
   resources :users
-  resources :genres
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
