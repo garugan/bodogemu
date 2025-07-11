31725580022ea51d193e3d492242a0779ac9a094
garugan <196047075+garugan@users.noreply.github.com>
2025-07-11 23:18:31 +0000

    []

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    if profile_image.attached?
+      profile_image.variant(resize_to_limit: [width, height]).processed
+    else
+      "https://mypf-bucket.s3.ap-northeast-1.amazonaws.com/no_image.jpg"
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
   def self.guest
