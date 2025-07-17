d1706333cef94ebe9ff456ec07e9139525da526a
garugan <196047075+garugan@users.noreply.github.com>
2025-07-17 04:28:40 +0000

    [Update]review_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 devise_for :admins, path: 'admin', controllers: {
   sessions: 'admin/admins/sessions'
-}
+}  
 
-  devise_for :users
+    devise_for :users
+    root to: "homes#top"
+    resources :games do
+      resources :reviews do
+        resources :review_comments
+      end
+    end
+    resources :users
+    resources :reviews
+    resources :genres
+    get "/search", to: "searches#search"
+    post 'guest_login', to: 'sessions#guest_login'
 
   namespace :admin do
     root to: 'dashboards#index'
@@ -13,17 +24,5 @@ devise_for :admins, path: 'admin', controllers: {
     resources :reviews
   end
 
-  root to: "homes#top"
-  get "/search", to: "searches#search"
-  post 'guest_login', to: 'sessions#guest_login'
-
-  resources :games do
-    resources :reviews do
-      resources :review_comments
-    end
-  end
-  resources :users
-  resources :reviews
-  resources :genres
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
